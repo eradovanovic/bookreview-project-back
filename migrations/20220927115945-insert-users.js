450e2252414d21bc0f6d563963873ce423b98edc
Emilija Radovanovic <emilija@77-46-141-236.static.isp.telekom.rs>
2023-01-30 12:30:01 +0100

    Code fix

--- migrations/20220927115945-insert-users.js
+++ migrations/20220927115945-insert-users.js
@@ -15,13 +15,6 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db, callback) {
-  // const promises = [];
-  // promises.push([
-  //     db.insert('users', { username : 'admin', password : 'Passw0rd!', type: 1 }),
-  //     db.insert('users', { username : 'jane', name: 'Jane', surname: 'Doe', email:'jane@mail.com', password : 'Passw0rd!', type: 2 }),
-  //
-  // ])
-  // return Promise.all(promises)
   const sql = "INSERT INTO users (username, password, type, name, surname, email, photo) values " +
       "('admin', 'Passw0rd!', 1, null, null, null, null)," +
       "('jane', 'Passw0rd!', 2, 'Jane', 'Doe', 'jane@gmail.com', 'https://randomuser.me/api/portraits/women/79.jpg')," +
